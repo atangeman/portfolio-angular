b5af17d7a74f9131692e937a41185ea5b528526e
atangeman <andytangeman@gmail.com>
2017-04-09 01:22:26 -0700

    worked on sidebar

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { RepoService } from '../repos/repo.service';
+import { Observable } from 'rxjs/Observable';
+import { Repo } from '../repos/repo.model';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+    title = 'Project Repositories';
+  errorMessage: string;
+  repos: Observable<Repo[]>;
+ // start button control
+  public isCollapsed: boolean = false;
+
+  // end button control
+  constructor(private repoService: RepoService) {
+    this.getRepos();
+  }
+
+  getRepos(): void {
+    this.repoService.getRepos().subscribe(repos => {
+      this.repos = repos;
+    });
+    console.log(this.repos);
+  }
+
+}
