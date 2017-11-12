67544564ddb5cb4e4692823e415d088cfbef2c27
atangeman <andytangeman@gmail.com>
2017-11-12 14:45:59 -0800

    config adjustment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { RepoService } from './repos/repo.service';
-import { Observable } from 'rxjs/Observable';
-import { Repo } from './repos/repo.model';
 
 import 'rxjs/add/operator/map';
 @Component({
@@ -10,23 +7,5 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Project Repositories';
-  errorMessage: string;
-  repos: Observable<Repo[]>;
-  numbers: string[];
- // start button control
-  public isCollapsed: boolean = false;
 
-  // end button control
-  constructor(private repoService: RepoService) {
-    this.numbers = Array.from(Array(100),(x,i)=>String(i+1));
-    this.getRepos();
-  }
-
-  getRepos(): void {
-    this.repoService.getRepos().subscribe(repos => {
-      this.repos = repos;
-    });
-    console.log(this.repos);
-  }
 }
