4f1213bb4e11bcd7f70cf9e108a32540257431a7
atangeman <andytangeman@gmail.com>
2020-10-04 12:40:31 -0700

    add extra checks to repo list

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,7 +8,9 @@ import { Repo } from '../repos/repo.model';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
+
 export class SidebarComponent {
+
   title = 'Project Repositories';
   errorMessage: string;
   repos: Repo[] = [];
@@ -16,17 +18,21 @@ export class SidebarComponent {
   public isCollapsed: boolean = false;
   public customClass: string = 'customClass';
   // end button control
+
   constructor(private repoService: RepoService) {
     //this.getRepos();
   }
 
   async getRepos(): Promise<void> {
-    let repoList = await this.repoService.getRepos();
-    for(let repo of repoList) {
+    if (this.repos.length < 1) {
+      let repoList = await this.repoService.getRepos();
+      for (let repo of repoList) {
         let list: string[] = await this.repoService.getRepoTags(repo.name);
-        if(list.indexOf("portfolio") > -1) {
-          this.repos.push(repo);
-      }
-    }    
+        if (list.indexOf("portfolio") > -1 && 
+           (this.repos.map(function(e) { return e.name; }).indexOf(repo.name) == -1)) {
+            this.repos.push(repo);
+        }
+      }    
+    }
   }
 }
